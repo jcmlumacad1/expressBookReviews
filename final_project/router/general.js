09ed79a842d2fdfd0a2b1269b818c16e440c50b4
jcmlumacad1 <44424424+jcmlumacad1@users.noreply.github.com>
2024-08-27 17:25:36 +0800

    rewrite using promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,40 +28,66 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  res.send(JSON.stringify(books,null,4));
+  const getAllBooks = () => new Promise((resolve, reject) => {
+    resolve(books)
+  })
+
+  return getAllBooks().then(books => {
+    res.send(JSON.stringify(books,null,4));
+  });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-  res.send(books[isbn]);
- });
+  const getBookByIsbn = () => new Promise((resolve, reject) => {
+    resolve(books[isbn])
+  })
+
+  return getBookByIsbn().then(book => {
+    res.send(book);
+  });
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  const author = req.params.author;
-  const filteredBooks = {};
-  Object.entries(books).forEach(([key, book]) => {
-    if (author == book.author) {
-      filteredBooks[key] = book;
-    }
+  const getBooksByAuthor = () => new Promise((resolve, reject) => {
+    const author = req.params.author;
+    const filteredBooks = {};
+    Object.entries(books).forEach(([key, book]) => {
+      if (author == book.author) {
+        filteredBooks[key] = book;
+      }
+    });
+    
+    resolve(filteredBooks);
+  })
+
+  return getBooksByAuthor().then(filteredBooks => {
+    res.send(filteredBooks);
   });
-  res.send(filteredBooks);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  const title = req.params.title;
-  const filteredBooks = {};
-  Object.entries(books).forEach(([key, book]) => {
-    if (title == book.title) {
-      filteredBooks[key] = book;
-    }
+  const getBooksByTitle = () => new Promise((resolve, reject) => {
+    const title = req.params.title;
+    const filteredBooks = {};
+    Object.entries(books).forEach(([key, book]) => {
+      if (title == book.title) {
+        filteredBooks[key] = book;
+      }
+    });
+    
+    resolve(filteredBooks);
+  })
+
+  return getBooksByTitle().then(filteredBooks => {
+    res.send(filteredBooks);
   });
-  res.send(filteredBooks);
 });
 
 //  Get book review
